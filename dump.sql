b51bd02975472f1c8398a87bb6a2995ffb6c374a
Ayfri <pierre.ayfri@gmail.com>
2023-10-03 17:06:54 +0200

    refactor(db): Make rarity & price nullable, all ids are now bigint, remove holofoil,

--- dump.sql
+++ dump.sql
@@ -4,81 +4,78 @@
 -- ------------------------------------------------------
 -- Server version	10.6.5-MariaDB
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE = @@TIME_ZONE */;
-/*!40103 SET TIME_ZONE = '+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS, UNIQUE_CHECKS = 0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0 */;
-/*!40101 SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES = @@SQL_NOTES, SQL_NOTES = 0 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `cards`
 --
 
-/*!40101 SET @saved_cs_client = @@character_set_client */;
+DROP TABLE IF EXISTS `cards`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `cards`
-(
-    `id`         int(10) unsigned     NOT NULL,
-    `pokemon_id` int(10) unsigned     NOT NULL,
-    `set_id`     int(10) unsigned     NOT NULL,
-    `price`      float(8, 2) unsigned NOT NULL,
-    `image_url`  varchar(1000)                 DEFAULT NULL,
-    `holofoil`   tinyint(1)           NOT NULL DEFAULT 0,
-    PRIMARY KEY (`id`),
-    KEY `fk_cards_sets` (`set_id`),
-    KEY `fk_cards_pokemons` (`pokemon_id`),
-    CONSTRAINT `fk_cards_pokemons` FOREIGN KEY (`pokemon_id`) REFERENCES `pokemons` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT `fk_cards_sets` FOREIGN KEY (`set_id`) REFERENCES `sets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB
-  DEFAULT CHARSET = latin1;
+CREATE TABLE `cards` (
+  `id` bigint(20) unsigned NOT NULL,
+  `pokemon_id` bigint(20) unsigned NOT NULL,
+  `set_id` bigint(20) unsigned NOT NULL,
+  `price` float(8,2) unsigned DEFAULT NULL,
+  `image_url` varchar(1000) DEFAULT NULL,
+  `rarity` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_cards_sets` (`set_id`),
+  KEY `fk_cards_pokemons` (`pokemon_id`),
+  CONSTRAINT `fk_cards_pokemons_0` FOREIGN KEY (`pokemon_id`) REFERENCES `pokemons` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_cards_sets_0` FOREIGN KEY (`set_id`) REFERENCES `sets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `pokemons`
 --
 
-/*!40101 SET @saved_cs_client = @@character_set_client */;
+DROP TABLE IF EXISTS `pokemons`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `pokemons`
-(
-    `id`          int(10) unsigned NOT NULL,
-    `description` varchar(1000)    NOT NULL,
-    `name`        varchar(100)     NOT NULL,
-    `numero`      int(10) unsigned NOT NULL,
-    `type`        varchar(50)      NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = latin1;
+CREATE TABLE `pokemons` (
+  `id` bigint(20) unsigned NOT NULL,
+  `description` varchar(1000) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `numero` int(10) unsigned NOT NULL,
+  `type` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `sets`
 --
 
-/*!40101 SET @saved_cs_client = @@character_set_client */;
+DROP TABLE IF EXISTS `sets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `sets`
-(
-    `id`        int(10) unsigned NOT NULL,
-    `name`      varchar(100)     NOT NULL,
-    `image_url` varchar(1000)    NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = latin1;
+CREATE TABLE `sets` (
+  `id` bigint(20) unsigned NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `image_url` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE = @OLD_TIME_ZONE */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE = @OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS = @OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES = @OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-03  0:10:52
+-- Dump completed on 2023-10-03 17:02:01
