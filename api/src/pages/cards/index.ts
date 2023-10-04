e3727b3b7a9c182c21fed377b43d580d4ca44b6f
antaww <anta.pizzetta@gmail.com>
2023-10-04 22:11:00 +0200

    feat(filters): Add numero filter (not available in front) + display every cards with filters.

--- api/src/pages/cards/index.ts
+++ api/src/pages/cards/index.ts
@@ -1,45 +1,45 @@
-import type {APIRoute} from "astro";
 import prisma from '../../prisma.ts';
 import {jsonResponse} from "../../utils.ts";
+import type {APIRoute} from "astro";
 
 type GetParameters = APIRoute & {
-	d: {
-		searchParams: {
-			id: string;
-			types: string;
-			name: string;
-			rarity: string;
-			set: string;
-		}
-	};
+    d: {
+        searchParams: {
+            id: string;
+            types: string;
+            name: string;
+            rarity: string;
+            set: string;
+        }
+    };
 
-	url: URL;
+    url: URL;
 };
 
 export async function GET({url}: GetParameters) {
-	const params = Object.fromEntries(url.searchParams.entries());
+    console.log(url)
+    const params = Object.fromEntries(url.searchParams.entries());
 
-	const cardFilter: Record<string, any> = {};
-	if (params.types) cardFilter.types = {contains: params.types};
-	if (params.rarity) cardFilter.rarity = {contains: params.rarity};
+    const cardFilter: Record<string, any> = {};
+    if (params.types) cardFilter.types = {contains: params.types}
+    if (params.rarity) cardFilter.rarity = {contains: params.rarity}
 
-	const pokemonFilter: Record<string, any> = {};
-	if (params.name) pokemonFilter.name = {contains: params.name};
-	if (params.numero) pokemonFilter.numero = {contains: params.numero};
+    const pokemonFilter: Record<string, any> = {};
+    if (params.name) pokemonFilter.name = {contains: params.name}
+    if (params.numero) pokemonFilter.numero = {equals: parseInt(params.numero)}
 
-	const setFilter: Record<string, any> = {};
-	if (params.set) setFilter.name = {contains: params.set};
-	const cards = await prisma().cards.findMany({
-		include: {
-			pokemon: true,
-			set: true,
-		},
-		where: {
-			pokemon: pokemonFilter,
-			set: setFilter,
-			...cardFilter,
-		}
-		/*where: cardFilter*/
-	});
-	return jsonResponse(cards);
+    const setFilter: Record<string, any> = {};
+    if (params.set) setFilter.name = {contains: params.set}
+    const cards = await prisma().cards.findMany({
+        include: {
+            pokemon: true,
+            set: true,
+        },
+        where: {
+            pokemon: pokemonFilter,
+            set: setFilter,
+            ...cardFilter,
+        }
+    });
+    return jsonResponse(cards);
 }
