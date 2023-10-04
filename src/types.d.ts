a568dc2003a0b99cbcd65bd5335fee9114e28100
Bahsiik <o.mist1303@gmail.com>
2023-10-05 00:32:33 +0200

    feat(interfaces): Put the interfaces in another file.

---
+++ src/types.d.ts
@@ -0,0 +1,21 @@
+export interface Card {
+    imageUrl: string;
+    types: string;
+    rarity: string;
+    price: number;
+    pokemon: Pokemon;
+    set: Set;
+}
+
+export interface Pokemon {
+    numero: number;
+    name: string;
+    description: string;
+    imageUrl: string;
+    type: string;
+}
+
+export interface Set {
+    name: string;
+    imageUrl: string;
+}
