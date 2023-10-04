c2475795087d32563a8e3747fa2ab7de22633ecc
antaww <anta.pizzetta@gmail.com>
2023-10-04 15:31:10 +0200

    feat(api): Add api endpoints (types, name, rarity, set).

--- api/src/pages/cards/index.ts
+++ api/src/pages/cards/index.ts
@@ -1,12 +1,36 @@
 import prisma from '../../prisma.ts';
 import {jsonResponse} from "../../utils.ts";
+import type {APIRoute} from "astro";
 
-export async function GET() {
-	const cards = await prisma().cards.findMany({
-		include: {
-			pokemon: true,
-			set: true,
-		}
-	});
-	return jsonResponse(cards);
+type GetParameters = APIRoute & {
+    d: {
+        searchParams: {
+            id: string;
+            types: string;
+            name: string;
+            rarity: string;
+            set: string;
+        }
+    };
+
+    url: URL;
+};
+
+export async function GET({url}: GetParameters) {
+    console.log(url)
+    const params = Object.fromEntries(url.searchParams.entries());
+
+    const object: Record<string, any> = {};
+    if (params.types) object.types = {contains: params.types}
+    if (params.name) object.name = {contains: params.name}
+    if (params.rarity) object.rarity = {contains: params.rarity}
+    if (params.set) object.set = {contains: params.set}
+    const cards = await prisma().cards.findMany({
+        include: {
+            pokemon: true,
+            set: true,
+        },
+        where: object,
+    });
+    return jsonResponse(cards);
 }
