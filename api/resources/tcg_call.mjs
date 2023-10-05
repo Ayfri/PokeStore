04a5baaa05adf3780510a711756e0cd33c7eff97
Ayfri <pierre.ayfri@gmail.com>
2023-10-05 17:54:40 +0200

    fix(sets): Only fetch sets of cards.

--- api/resources/tcg_call.mjs
+++ api/resources/tcg_call.mjs
@@ -1,11 +1,11 @@
-import pokemon from 'pokemontcgsdk'
+import pokemon from 'pokemontcgsdk';
 import {configDotenv} from "dotenv";
 // noinspection ES6UnusedImports
 import fs from "fs";
 
 
 const dotenv = configDotenv({
-	path: '../.env'
+	path: '../.env',
 });
 
 pokemon.configure({apiKey: dotenv.parsed['POKEMON_TCG_API_KEY']});
@@ -17,7 +17,7 @@ async function getPokemon(index) {
 	const cards = await pokemon.card.all({
 		q: `nationalPokedexNumbers:${index}`,
 		orderBy: 'nationalPokedexNumbers',
-		select: 'name,rarity,images,set,cardmarket,types,nationalPokedexNumbers'
+		select: 'name,rarity,images,set,cardmarket,types,nationalPokedexNumbers',
 	});
 
 	console.log(`Pokedex: ${index}/151, caught ${cards[0].name} ! (${cards.length} cards)`);
@@ -28,10 +28,10 @@ async function getPokemon(index) {
 		image: card.images.large,
 		numero: card.nationalPokedexNumbers.join(', '),
 		set_name: card.set.name,
-		price: card?.cardmarket?.prices?.averageSellPrice || card?.tcgplayer?.prices?.holofoil?.market
-			|| card?.tcgplayer?.prices?.reverseHolofoil?.market || card?.tcgplayer?.prices?.normal?.market
-			|| card?.tcgplayer?.prices?.["1stEditionHolofoil"]?.market || card?.tcgplayer?.prices?.["1stEditionNormal"]?.market,
-		types: card.types.join(', ')
+		price: card?.cardmarket?.prices?.averageSellPrice || card?.tcgplayer?.prices?.holofoil?.market || card?.tcgplayer?.prices?.reverseHolofoil?.market ||
+			card?.tcgplayer?.prices?.normal?.market || card?.tcgplayer?.prices?.["1stEditionHolofoil"]?.market ||
+			card?.tcgplayer?.prices?.["1stEditionNormal"]?.market,
+		types: card.types.join(', '),
 	})).filter((_, index) => index <= 10).sort((a, b) => b.price - a.price);
 
 	return filteredCards;
@@ -42,21 +42,29 @@ async function getSet() {
 	 * @type {import('pokemontcgsdk').Set[]}
 	 */
 	const sets = await pokemon.set.all({
-		select: 'name,images'
+		select: 'name,images',
 	});
 
 	console.log(`Found ${sets.length} sets !`);
 
 	sets.map(set => ({
 		name: set.name,
-		logo: set.images.logo
+		logo: set.images.logo,
 	}));
 
+	// filter by cards
+	const cardsJson = await import('./cards.json', {
+		assert: {type: 'json'},
+	});
+
+	const cards = cardsJson.default.flat();
+	const setsWithCards = sets.filter(set => cards.some(card => card.set_name === set.name));
+
 	// remove duplicate names
-	const uniqueNames = [...new Set(sets.map(set => set.name))];
+	const uniqueNames = [...new Set(setsWithCards.map(set => set.name))];
 	return uniqueNames.map(name => ({
 		name: name,
-		logo: sets.find(set => set.name === name).images.logo
+		logo: sets.find(set => set.name === name).images.logo,
 	}));
 }
 
@@ -72,4 +80,5 @@ async function getSet() {
 // UNCOMMENT TO GET SETS
 
 // const sets = await getSet();
+// console.log(`Filtered sets, writing ${sets.length} sets !`);
 // fs.writeFileSync('sets.json', JSON.stringify(sets, null, 2));
