4a87ddd594a3fd6597409008d73e6b580503ba09
antaww <anta.pizzetta@gmail.com>
2023-10-06 11:16:07 +0200

    feat(admin): Full working version of edit pokémon.

--- api/src/pages/admin/pokemon.ts
+++ api/src/pages/admin/pokemon.ts
@@ -6,7 +6,7 @@ import {errorResponse, formData} from '../../utils.ts';
 interface Pokemon {
 	description: string;
 	name: string;
-	numero: number;
+	numero: string;
 	type: string;
 }
 
@@ -16,38 +16,60 @@ export async function POST({request, url}: APIContext) {
 	const searchParam = url.searchParams.get('button');
 	let {description, name, numero, type} = await formData<Pokemon>(request);
 	console.log(`description: ${description}, name: ${name}, numero: ${numero}, type: ${type}`);
-	numero = parseInt(numero.toString());
+	const number = Number.parseInt(numero.toString());
+	if (number.toString() !== numero) return errorResponse('Numero must be a number.');
 
-	if(!description || !name || !numero || !type) {
+	if (!description || !name || !numero || !type) {
 		return errorResponse('Missing fields.');
 	}
 
-	/*todo: edit & remove params*/
+	/*todo: remove params*/
 	const existingPokemon = await prismaClient.pokemons.findFirst({
 		where: {
 			OR: [
-				{numero},
+				{numero: number},
 				{name},
 				{description},
 			],
 		}
 	});
 
-	if(searchParam === 'add' && existingPokemon) {
-		if (existingPokemon.description === description) return errorResponse('Description already exists.');
-		if (existingPokemon.name === name) return errorResponse('Name already exists.');
-		if (existingPokemon.numero === numero) return errorResponse('Numero already exists.');
+	switch (searchParam) {
+		case 'add':
+			if (existingPokemon) {
+				if (existingPokemon.description === description) return errorResponse('Description already exists.');
+				if (existingPokemon.name === name) return errorResponse('Name already exists.');
+				if (existingPokemon.numero === number) return errorResponse('Numero already exists.');
+			} else {
+				const pokemon = await prismaClient.pokemons.create({
+					data: {
+						id: newId(), description, name, numero: number, type,
+					},
+				});
+
+				if (!pokemon) return errorResponse('Unknown error.');
+				return new Response('Pokemon successfully created!', {
+					status: 201,
+				});
+			}
+			break;
+
+		case 'edit':
+			if (!existingPokemon) return errorResponse('Pokemon does not exist.');
+			if (isNaN(parseInt(number.toString()))) return errorResponse('Numero must be a number.');
+			if (existingPokemon.description === description && existingPokemon.name === name && existingPokemon.numero === number && existingPokemon.type === type) return errorResponse('No changes were made.');
+			const pokemon = await prismaClient.pokemons.update({
+				where: {
+					id: existingPokemon.id,
+				},
+				data: {
+					description, name, numero: number, type,
+				}
+			});
+
+			if (!pokemon) return errorResponse('Unknown error.');
+			return new Response('Pokemon successfully edited!', {
+				status: 201,
+			});
 	}
-
-	const pokemon = await prismaClient.pokemons.create({
-		data: {
-			id: newId(), description, name, numero, type,
-		},
-	});
-
-
-	if (!pokemon) return errorResponse('Unknown error.');
-	return new Response('Pokemon successfully created!', {
-		status: 201,
-	})
 }
