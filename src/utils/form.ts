2ef9818790a2cc723cee73b0cb6cde5121e88ff8
Ayfri <pierre.ayfri@gmail.com>
2023-10-06 08:45:50 +0200

    feat(forms): Send event object into callback.

--- src/utils/form.ts
+++ src/utils/form.ts
@@ -14,19 +14,21 @@ export function displayError(error: string, form: HTMLFormElement = document.que
 }
 
 export function handleForm<T = any>(
-	onResult: (result: string, response: Response, form: HTMLFormElement) => void,
+	onResult: (result: string, response: Response, form: HTMLFormElement, event: SubmitEvent) => void,
 	getAsJson?: false,
 	form?: HTMLFormElement,
 ): void;
 export function handleForm<T = any>(
-	onResult: (result: T, response: Response, form: HTMLFormElement) => void,
+	onResult: (result: T, response: Response, form: HTMLFormElement, event: SubmitEvent) => void,
 	getAsJson?: true,
 	form?: HTMLFormElement,
 ): void;
-export function handleForm<T = any>(onResult: (result: T, response: Response, form: HTMLFormElement) => void = () => {
-	console.log('Form submitted');
-}, getAsJson: boolean = true, form: HTMLFormElement = document.querySelector('form') as HTMLFormElement) {
-	form.addEventListener('submit', async (event) => {
+export function handleForm<T = any>(
+	onResult: (result: T, response: Response, form: HTMLFormElement, event: SubmitEvent) => void = () => console.log('Form submitted'),
+	getAsJson: boolean = true,
+	form: HTMLFormElement = document.querySelector('form') as HTMLFormElement,
+) {
+	form.addEventListener('submit', async event => {
 		event.preventDefault();
 		const formData = new FormData(form);
 		const response = await fetch(form.action, {
@@ -34,6 +36,6 @@ export function handleForm<T = any>(onResult: (result: T, response: Response, fo
 			body: formData,
 		});
 		const result = getAsJson ? await response.json() : await response.text();
-		onResult(result, response, form);
+		onResult(result, response, form, event);
 	});
 }
