f06bddec9a1bedc76b7841c45720321e83866388
Ayfri <pierre.ayfri@gmail.com>
2023-10-08 12:54:58 +0200

    fix(api): Fix going to logout route without being connected.

--- api/src/pages/auth/logout.ts
+++ api/src/pages/auth/logout.ts
@@ -1,6 +1,7 @@
 import type {APIContext} from 'astro';
 import prisma from '../../prisma.ts';
 import {SESSION_TOKEN} from '../../constants.ts';
+import {errorResponse} from '../../utils.ts';
 
 const prismaClient = prisma();
 
@@ -8,13 +9,15 @@ export async function POST({cookies}: APIContext) {
 	if (!cookies.has(SESSION_TOKEN)) return new Response('Unauthorized.', {status: 401});
 	const sessionToken = cookies.get(SESSION_TOKEN);
 
+	if (!sessionToken) return errorResponse('Unauthorized.', 401);
+
 	const session = await prismaClient.sessions.count({
 		where: {
 			id: sessionToken.value,
 		},
 	});
 
-	if (!session) return new Response('Unauthorized.', {status: 401});
+	if (!session) return errorResponse('Unauthorized.', 401);
 
 	await prismaClient.sessions.delete({
 		where: {
