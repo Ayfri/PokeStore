71c28c70ee9b1448b6cb04c2927f3e2ab331ceb8
Ayfri <pierre.ayfri@gmail.com>
2023-10-08 12:49:37 +0200

    feat(types): Improve typings of card filters route.

--- api/src/pages/cards/index.ts
+++ api/src/pages/cards/index.ts
@@ -2,32 +2,39 @@ import prisma from '../../prisma.ts';
 import {jsonResponse} from "../../utils.ts";
 import type {APIRoute} from "astro";
 
-type GetParameters = APIRoute & {
-	d: {
-		searchParams: {
-			id: string;
-			types: string;
-			name: string;
-			rarity: string;
-			set: string;
-		}
-	};
+type SearchParameters = {
+	numero: string;
+	types: string;
+	name: string;
+	rarity: string;
+	set: string;
+}
 
+type GetParameters = APIRoute & {
 	url: URL;
 };
 
-export async function GET({url}: GetParameters) {
-	const params = Object.fromEntries(url.searchParams.entries());
+type FilterValue = {
+	contains?: string;
+	equals?: number;
+}
 
-	const cardFilter: Record<string, any> = {};
+type Filter = {
+	[key: string]: FilterValue;
+}
+
+export async function GET({url}: GetParameters) {
+	const params = Object.fromEntries(url.searchParams.entries()) as SearchParameters;
+
+	const cardFilter: Filter = {};
 	if (params.types) cardFilter.types = {contains: params.types};
 	if (params.rarity) cardFilter.rarity = {contains: params.rarity};
 
-	const pokemonFilter: Record<string, any> = {};
+	const pokemonFilter: Filter = {};
 	if (params.name) pokemonFilter.name = {contains: params.name};
 	if (params.numero) pokemonFilter.numero = {equals: parseInt(params.numero)};
 
-	const setFilter: Record<string, any> = {};
+	const setFilter: Filter = {};
 	if (params.set) setFilter.name = {contains: params.set};
 	const cards = await prisma().cards.findMany({
 		include: {
