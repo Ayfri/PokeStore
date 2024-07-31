75e07e176b9675550e073e35ac12daba151e7d75
Ayfri <pierre.ayfri@gmail.com>
2024-07-31 16:40:03 +0200

    fix(config): Update `astro.config.mjs` output to `static`, remove `svelte` integration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import {defineConfig} from 'astro/config';
 
-import svelte from "@astrojs/svelte";
-
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	integrations: [svelte()],
+	output: 'static',
 });
